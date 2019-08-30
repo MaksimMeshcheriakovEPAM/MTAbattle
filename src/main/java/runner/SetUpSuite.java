531282f79e94447ded3203c4962889e2a2e43d91
Maksim Meshcheriakov <Maksim_Meshcheriakov@epam.com>
2019-08-30 16:06:19 +0300

    try to get a current testng group value

--- src/main/java/runner/SetUpSuite.java
+++ src/main/java/runner/SetUpSuite.java
@@ -1,17 +1,20 @@
 package runner;
 
 
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 
 public class SetUpSuite {
 
-    @BeforeClass()
-   public void setUpSession(){
+    @BeforeClass(groups = {"Android, iOS"}, alwaysRun = true)
+   public void setUpSession(ITestAnnotation annotation){
+        System.out.println("Group Name is --" + annotation.getGroups()) ;
 
    }
 
    public void tearDownSession(){
+        System.out.println("Test done");
 
    }
+
 
 }
--- src/test/java/scenarios/CalcTest.java
+++ src/test/java/scenarios/CalcTest.java
@@ -1,7 +1,14 @@
 package scenarios;
 
+import org.testng.annotations.Test;
 import runner.SetUpSuite;
 
+
 public class CalcTest extends SetUpSuite {
+
+    @Test
+    public void firstTest(){
+        System.out.println("Test itself");
+    }
 
 }
